70e3bf38f2b810e2e102de297c21593a69955e34
github username <github email address>
2018-04-26 08:18:25 -0400

    removed chart

--- src/Graphics.js
+++ src/Graphics.js
@@ -5,19 +5,19 @@ class Graphics extends React.Component {
 debugger
   // ctx = document.getElementById('myChart')
   // .getElementById('myChart').getContext('2d');
-  chart = new Chart(this.ctx, {
-    type: 'line',
-     data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [{
-            label: "My First dataset",
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45],
-        }]
-     },
-     options: {}
-  });
+  // chart = new Chart(this.ctx, {
+  //   type: 'line',
+  //    data: {
+  //       labels: ["January", "February", "March", "April", "May", "June", "July"],
+  //       datasets: [{
+  //           label: "My First dataset",
+  //           backgroundColor: 'rgb(255, 99, 132)',
+  //           borderColor: 'rgb(255, 99, 132)',
+  //           data: [0, 10, 5, 2, 20, 30, 45],
+  //       }]
+  //    },
+  //    options: {}
+  // });
   render(){
     console.log("document", document)
     console.log("document.getElementByIdmyChart", document.getElementById('myChart').getContext('2d'))
@@ -28,9 +28,9 @@ debugger
         <p>one counter that says the most losses you incurred in a row....maybe one that shows winning streak</p>
         <p>One graph that shows the distribution of red and black spins</p>
         <img alt="spinning roulette wheel" src="wheel.gif" />
-        <canvas id="myChart"></canvas>
+        {/* <canvas id="myChart"></canvas>
           {/* ctx = document.getElementById('myChart').getContext('2d'); */}
-        {this.chart}
+        {this.chart} */}
       </div>
     )
   }
