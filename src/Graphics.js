20135e24e82c2deba08aca089eea0333693d72eb
github username <github email address>
2018-04-26 09:03:51 -0400

    updated language

--- src/Graphics.js
+++ src/Graphics.js
@@ -6,9 +6,9 @@ class Graphics extends React.Component {
   render(){
     return (
       <div className="left">
-        <p>Graph and stats and shit</p>
+        <p>Graph and stats</p>
         <p>One graph that does a count of each spin</p>
-        <p>one counter that says the most losses you incurred in a row....maybe one that shows winning streak</p>
+        <p>one counter that says the most losses you incurred in a row....maybe one that shows winning streak!</p>
         <p>One graph that shows the distribution of red and black spins</p>
         <img alt="spinning roulette wheel" src="wheel.gif" />
 
